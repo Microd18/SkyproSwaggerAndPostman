d629465bf8c4339b8b23cca27fe4b649394d9140
Microd18 <Microd@inbox.ru>
2023-06-19 23:17:50 +0500

    First commit!

---
+++ src/main/java/ru/hogwarts/school/SwaggerAndPostmanApplication.java
@@ -0,0 +1,15 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerAndPostmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerAndPostmanApplication.class, args);
+
+        System.out.println("Йоу");
+    }
+
+}
